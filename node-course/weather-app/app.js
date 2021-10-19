722765df8371ade2fb4ea804986d88c96753d70a
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-19 21:27:58 +0800

    unit parameter(f)

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -3,5 +3,5 @@ const request = require('request')
 const url = 'http://api.weatherstack.com/current?access_key=219da365f95c1be075845313a174325f&query=37.8267,-122.4233&units=f'
 
 request({ url: url, json: true }, (error, response) => { 
-    console.log('It is Currently ' + response.body.current.temperature + 'degress out. It feels like ' + response.body.current.feelslike + ' % chance of rain')
+    console.log(response.body.current.weather_descriptions[0] + ". It is Currently " + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degress out')
 })
