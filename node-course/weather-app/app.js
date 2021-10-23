271ed5e02fed1fc1a56883fa37751146cb4f2313
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-23 22:56:32 +0800

    object destructing

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -3,19 +3,19 @@ const forecast = require('./utils/forecast')
 
 const address = process.argv[2]
 if (!address) {
-    console.log('Please provide a adress')
+    console.log('Please provide an address')
 } else {
-    geocode(address , (error, data) => {
+    geocode(address , (error, {longitude, latitude, location } = {}) => {
         if (error) {
             return console.log(error)
         }
-   
-        forecast(data.longitude, data.latitude, (error, forecastData) => {
+    
+        forecast(longitude, latitude, (error, forecastData) => {
             if (error) {
                 return console.log(error)
             }
    
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
          }) 
      })
--- node-course/weather-app/utils/forecast.js
+++ node-course/weather-app/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=219da365f95c1be075845313a174325f&query=' + longitude + ',' + latitude +'&units=f' 
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body}) => {
             if (error) {
                     callback('unable to connect to weather service', undefined)
-            } else if (response.body.error) {
+            } else if (body.error) {
                     callback('unable to find location', undefined)
             } else {
-                    callback(undefined, response.body.current.weather_descriptions[0] + " It is Currently " + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degress out ')
+                    callback(undefined, body.current.weather_descriptions[0] + " It is Currently " + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out ')
             }
           })
     }
