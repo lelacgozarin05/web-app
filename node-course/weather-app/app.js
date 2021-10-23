7b4065b27747992a5b4eccc9d6c812a23b83f388
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-23 12:48:40 +0800

    callback chaining

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,27 +1,22 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-
-//  const url = 'http://api.weatherstack.com/current?access_key=219da365f95c1be075845313a174325f&query=37.8267,-122.4233&units=f'
-
-// request({ url: url, json: true }, (error, response) => { 
-//     if (error){
-//         console.log('Unable to Connect to weather service')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//     console.log(response.body.current.weather_descriptions[0] + " It is Currently " + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degress out ')
-//     } 
-// })
-
-
- geocode('Boston' , (error, data) => {
-     console.log('error',Error)
-     console.log('data',data)
-  })
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  }) 
+const address = process.argv[2]
+if (!address) {
+    console.log('Please provide a adress')
+} else {
+    geocode(address , (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+   
+        forecast(data.longitude, data.latitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+   
+            console.log(data.location)
+            console.log(forecastData)
+         }) 
+     })
+}
