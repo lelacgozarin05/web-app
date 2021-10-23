18cd06b32d7ad30bee9da4594b0d4e400b709dfb
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-23 10:46:58 +0800

    callbacks

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -12,21 +12,42 @@ const request = require('request')
 //     } 
 // })
  
-// geocoding
+// // geocoding
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoibGVsYWNnb3phcmkwNTE5IiwiYSI6ImNrdXcwNWgxMzJia2QydnBnZWxjdmtlYWoifQ.bSqHCksoG8qFNvDYMfOjcw'
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoibGVsYWNnb3phcmkwNTE5IiwiYSI6ImNrdXcwNWgxMzJia2QydnBnZWxjdmtlYWoifQ.bSqHCksoG8qFNvDYMfOjcw'
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location service!')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search')
-    } else {
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to location service!')
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location. Try another search')
+//     } else {
 
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0] 
-    console.log(latitude,longitude)
-    }
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0] 
+//     console.log(latitude,longitude)
+//     }
     
-})
+// })
 
+const geocode = (address, callback) => {
+        const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibGVsYWNnb3phcmkwNTE5IiwiYSI6ImNrdXcwNWgxMzJia2QydnBnZWxjdmtlYWoifQ.bSqHCksoG8qFNvDYMfOjcw&limit=1'
+
+            request({ url: url, json: true }, (error, response) => {
+                if (error) {
+                    callback('Unable to Connect to Location Services', undefined)
+                } else if (response.body.features.length === 0) {
+                    callback('Unable to find Location. try another search', undefined)
+                } else {
+                       callback(undefined, {
+                           latitude: response.body.features[0].center[1],
+                           longitude: response.body.features[0].center[0],
+                           location: response.body.features[0].place_name
+                       })
+                }
+            })
+         }
+ geocode('Philadelphia',(error, data) => {
+     console.log('error',Error)
+     console.log('data',data)
+  })
