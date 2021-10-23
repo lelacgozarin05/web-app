8b97116366f084890aecb90b54fa4a69441d10e0
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-24 00:44:21 +0800

    web-serve

---
+++ node-course/web-server/src/app.js
@@ -0,0 +1,17 @@
+const { response } = require('express')
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('Hello express!')
+
+})
+
+app.get('/help', (req, res) =>{
+    res.send('Help page')
+})
+//app.com
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
