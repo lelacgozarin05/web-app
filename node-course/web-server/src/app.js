885925fa8f91c4479b05f94150df50b22af5def3
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-25 01:45:30 +0800

    noodemon

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -8,8 +8,17 @@ app.get('', (req, res) => {
 
 })
 
+
 app.get('/help', (req, res) =>{
     res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Your Weather')
 })
 //app.com
 app.listen(3000, () => {
