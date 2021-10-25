6cd2080a303445afd24aa3e3af2497dad6e605ff
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-25 16:34:49 +0800

    all html file

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,28 +1,10 @@
-const { response } = require('express')
+const path= require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-
-})
-
-
-app.get('/help', (req, res) => {
-    res.send([{ 
-        name: 'lelac'
-    }, {
-        name: 'maricar'
-    }])
-})
- 
-
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
-
+app.use(express.static(publicDirectoryPath))
 
 
 app.get('/weather', (req, res) => {
