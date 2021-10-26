01eabd6234d17b95278067dfccd270b27c324a13
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-26 14:36:11 +0800

    geocoding

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,6 +1,8 @@
 const path= require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -48,10 +50,22 @@ if (!req.query.address) {
     })
 }
 
-    res.send({
-        forecast: 'It is snowing',
-        location: 'USA',
-        address: req.query.address
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send ({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData)  => {
+            if (error) {
+                return res.send(error)
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
 
@@ -85,7 +99,7 @@ app.get('*', (req, res) => {
         name: 'lelac gozarin',
         errorMessage: 'Page not found'
     })
-})
+})   
 
 //app.com
 app.listen(3000, () => {
