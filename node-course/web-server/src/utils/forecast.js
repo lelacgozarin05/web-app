01b0f868a3ea17c0ab17097569c4c1939d00b3bf
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-26 23:00:48 +0800

    change of code

--- node-course/web-server/src/utils/forecast.js
+++ node-course/web-server/src/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('request')
 
-const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=219da365f95c1be075845313a174325f&query=' + longitude + ',' + latitude +'&units=f' 
+const forecast = (longitude, latitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=219da365f95c1be075845313a174325f&query=' + latitude + ',' + longitude +'&units=m' 
 
     request({ url: url, json: true }, (error, { body}) => {
             if (error) {
-                    callback('unable to connect to weather service', undefined)
+                    callback('Unable to connect to the Internet', undefined)
             } else if (body.error) {
-                    callback('unable to find location', undefined)
+                    callback('Unable to find location', undefined)
             } else {
-                    callback(undefined, body.current.weather_descriptions[0] + " It is Currently " + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out ')
+                    callback(undefined, body.current.temperature + '\xB0' + 'c' + ' ' + body.current.weather_descriptions [0] + '') // degress out. It feels like ' + body.current.feelslike + ' degress out ')
             }
           })
     }
