16c1de2806c6c59be4f45c3db59ddae9817bae62
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-26 15:19:50 +0800

    function parameters

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -50,7 +50,7 @@ if (!req.query.address) {
     })
 }
 
-    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
             return res.send ({ error })
         }
