49eb074f86a92d3b1a0b7f8b76acb1039e079ddd
Mary-Bon Delavin <delavinmarybon@gmail.com>
2021-10-31 18:08:16 +0800

    geocode

--- node-course/web-server/src/utils/forecast.js
+++ node-course/web-server/src/utils/forecast.js
@@ -1,18 +1,22 @@
-const request = require('request')
 
-const forecast = (longitude, latitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=219da365f95c1be075845313a174325f&query=' + latitude + ',' + longitude +'&units=m' 
-       
-    request({url: url, json: true }, (error, { body}) => {
-            if (error) {
-                    callback('Unable to connect to the Internet', Undefined)
-            } else if (body.error) {
-                    callback('Unable to find location', Undefined)
-            } else {
-        
-                    callback(undefined,body.location.localtime + '' + body.current.temperature + '\xB0' + 'c' + ' ' + body.current.weather_descriptions [0] + '') // degress out. It feels like ' + body.current.feelslike + ' degress out ')
-            }
-          })
-    }
-
+const request = require ("request")
+const forecast = (latitude,longitude,callback) =>{
+    const url = 'http://api.weatherstack.com/current?access_key=1c21a33636a0e20fa75ee3458fa4b47c&query='+latitude + ','+ longitude+'&units=m'
+    request({url, json:true},(error,{body}) => { if (error) {
+        callback('Sorry! Unable to connect to weather service!',undefined) 
+        }else if (body.error) {
+         callback('unable to find location',undefined)
+        }else{
+        callback(undefined, body.current.temperature + ' \xB0C      ' + body.current.weather_descriptions [0])
+        }
+        })
+        request({url, json:true},(error,{body}) => { if (error) {
+                callback('Sorry! Unable to connect to weather service!',undefined) 
+                }else if (body.error) {
+                 callback('Unable to find location',undefined)
+                }else{
+                callback(undefined, body.current.temperature + ' \xB0C      ' + body.current.weather_descriptions [0])
+                }
+        })
+}                
 module.exports = forecast
--- node-course/web-server/src/utils/geocode.js
+++ node-course/web-server/src/utils/geocode.js
@@ -1,21 +1,22 @@
-const request = require('request')
 
-const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibGVsYWNnb3phcmkwNTE5IiwiYSI6ImNrdXcwNWgxMzJia2QydnBnZWxjdmtlYWoifQ.bSqHCksoG8qFNvDYMfOjcw&limit=1'
+const request = require ("request")
 
-        request({url:url, json: true }, (error, {body}) => {
-            if (error) {
-                callback('Unable to Connect to Location Services', undefined)
-            } else if (body.features.length === 0) {
-                callback('Unable to find Location. Try another Search', undefined)
-            } else {
-                   callback(undefined, {
-                       latitude: body.features[0].center[0],
-                       longitude: body.features[0].center[1],
-                       location: body.features[0].place_name
-                   })
-            }
-        }) 
-     }
+const geocode = (address, callback) =>{
+const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1IjoiYXByaWwtMDIiLCJhIjoiY2t1dzd3dDk2MXQybjJ1cTl4OGJyZngwZiJ9.hFMRs47B0fKZ06av2qahQA'
 
-     module.exports = geocode
+request({url, json: true},(error,{body})=>{
+    if(error){
+       callback('Unable to connect to location services!',undefined)
+    }
+    else if (body.features.length==0) {
+       callback('unable to find location Try searching again!', undefined)
+    }else{
+       callback(undefined,{
+          latitude:body.features[0].center[1],
+          longitude:body.features[0].center[0],
+          location:body.features[0].place_name
+       })
+    }
+}) 
+}
+module.exports = geocode
