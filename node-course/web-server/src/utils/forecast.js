d886c9c83ca3959d64e624005b02e4a15bd5ff64
Mary-Bon Delavin <delavinmarybon@gmail.com>
2021-10-31 20:34:08 +0800

    about us

--- node-course/web-server/src/utils/forecast.js
+++ node-course/web-server/src/utils/forecast.js
@@ -1,7 +1,7 @@
 
 const request = require ("request")
 const forecast = (latitude,longitude,callback) =>{
-    const url = 'http://api.weatherstack.com/current?access_key=1c21a33636a0e20fa75ee3458fa4b47c&query='+latitude + ','+ longitude+'&units=m'
+    const url = 'http://api.weatherstack.com/current?access_key=eb2733f37563dd12392aa6ef1c9b3aac&query=12.67037,%20123.88527'+latitude + ','+ longitude+'&units=m'
     request({url, json:true},(error,{body}) => { if (error) {
         callback('Sorry! Unable to connect to weather service!',undefined) 
         }else if (body.error) {
