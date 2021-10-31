ac7c663af51518cdb39052ee7e6107a9f37e0d75
Mary-Bon Delavin <delavinmarybon@gmail.com>
2021-10-31 17:01:53 +0800

    change code 1

--- node-course/web-server/public/js/app.js
+++ node-course/web-server/public/js/app.js
@@ -1,31 +1,73 @@
 
-const weatherForm = document.querySelector('form')
+console.log('Client side javascript file is loaded')
+
+const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
-const messageTWo = document.querySelector('#message-2')
-const messageThree = document.querySelector('#message-3')
-//const messageFour = document.querySelector('#message-4')
-weatherForm.addEventListener('submit', (e) => {
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From javaScript'
+
+weatherform.addEventListener("submit",(e)=>{
     e.preventDefault()
 
     const location = search.value
 
-    messageOne.textContent = 'Loading...'
-    messageTWo.textContent = ''
-    messageThree.textContent = ''
+    messageOne.textContent = 'loading...'
+    messageTwo.textContent = ''
 
-    
-    fetch('/weather?address=' + location).then((response) =>{
-        response.json().then((data) => {
-            if (data.error) {
-                    messageOne.textContent = data.error
-            }else{
-
-                messageOne.textContent = data.location
-                messageOne.textContent = data.location
-                messageThree.textContent = data.forecast
-                
-            }
-        })
-    })
+    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+response.json().then((data)=>{
+if(data.error){
+messageOne.textContent = data.error
+}else{
+    messageOne.textContent = data.location
+    messageTwo.textContent = data.forecast
+}
 })
+})
+})
+
+//day
+let date = new Date();
+let dayOfWeekNumber = date.getDay();
+let nameOfDay;
+
+switch(dayOfWeekNumber){
+    case 0: 
+        nameOfDay = 'Sunday'
+        break;
+    case 1:
+        nameOfDay = 'Monday'
+        break;
+    case 2:
+        nameOfDay = 'Tuesday'
+        break;
+    case 3:
+        nameOfDay = 'Wednesday'
+        break;
+    case 4:
+        nameOfDay = 'Thursday'
+        break;
+    case 5:
+        nameOfDay = 'Friday'
+        break;
+    case 6:
+        nameOfDay = 'Saturday'
+        break;
+
+}
+//Display the day 
+let weekdayDiv = document.getElementById('weekday');
+weekdayDiv.innerHTML = `${nameOfDay}`;
+
+//Display the date
+const d = new Date();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("month").innerHTML = months[d.getMonth()];
+//Display 
+
+//display date
+const a = new Date();
+document.getElementById("day").innerHTML = a.getDate();
+//display date
